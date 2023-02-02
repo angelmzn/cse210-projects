ee5ceca8f9538cfef2caa353fe4db2c09b1503a8
Angel E. Manzano <angel.manzano777@hotmail.com>
2023-02-02 00:40:24 +0000

    Starting the journal project

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -5,5 +5,21 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Develop02 World!");
+        int answer = 0
+
+        while (answer != 4) {
+            
+
+            Console.WriteLine("Hello, Please tell me what you need to do")
+            Console.WriteLine("")
+            Console.WriteLine("To create a new file, press 1")
+            Console.WriteLine("To load an existing file, press 2")
+            Console.WriteLine("To delete an existing file, press 3")
+            Console.WriteLine("To quit, press 4")
+
+            if (answer = 1) {
+                
+            }
+        }
     }
 }
